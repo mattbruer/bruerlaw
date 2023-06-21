a124d2702d708eaa4746ec3122965446765f287c
Matt Bruer <mbruer@gmail.com>
2023-06-21 09:38:04 -0500

    fix typescript

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 type BannerProps = {
   background: string;
-  message: string;
+  message?: string;
 };
 
 const Banner = ({ background, message }: BannerProps) => {
@@ -15,6 +15,7 @@ const Banner = ({ background, message }: BannerProps) => {
         src={`/${background}`}
         alt="test"
         fill={true}
+        priority
       />
       <div id="banner-msg2">{message}</div>
     </div>
